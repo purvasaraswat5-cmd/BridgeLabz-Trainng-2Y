c182300aa42183f0b47d9bfe1ae0c0f1978decac
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-10 19:17:47 +0530

    First commit

---
+++ Day1_ProgrammingElements/javaprogrammingElement_level_02/QuotientRemainder.java
@@ -0,0 +1,24 @@
+package javaprogrammingElement_level_02;
+
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        try(Scanner input = new Scanner(System.in)){
+
+        System.out.print("Enter the first number: ");
+        int number1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int number2 = input.nextInt();
+
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+
+        System.out.println("The Quotient is " + quotient + " and Reminder is " + remainder +
+                           " of two number " + number1 + " and " + number2);
+
+        }
+    }
+    
+}
