58182071add6a8e1bb1147cc8635ff865f28f929
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-10 18:56:37 +0530

    First commit

---
+++ Day1_ProgrammingElements/Day1_ProgrammingElements_level_01/HarryAge.java
@@ -0,0 +1,11 @@
+package javaprogrammingElement_level_01;
+
+public class HarryAge {
+    public static void main(String[] args) {
+        int birth_year = 2000;
+        int current_year = 2024;
+        int current_age = current_year - birth_year;
+        System.out.println(current_age); 
+    }
+    
+}
