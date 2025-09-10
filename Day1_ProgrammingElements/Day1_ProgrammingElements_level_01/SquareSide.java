a7f1102c28fd791680ae32a7287ef5c2acb49f07
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-10 19:04:01 +0530

    First commit

---
+++ Day1_ProgrammingElements/Day1_ProgrammingElements_level_01/SquareSide.java
@@ -0,0 +1,19 @@
+package javaprogrammingElement_level_01;
+
+import java.util.Scanner;
+
+public class SquareSide {
+     public static void main(String[] args) {
+        try(Scanner input = new Scanner(System.in)){
+
+        System.out.print("Enter the perimeter : ");
+        double perimeter = input.nextDouble();
+
+        double side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+
+        }
+    }
+    
+}
