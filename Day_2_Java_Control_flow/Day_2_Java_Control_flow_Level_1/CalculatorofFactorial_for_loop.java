e70df522d97b537662313df41e8810061911a3be
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-10 19:25:07 +0530

    First commit

---
+++ Day_2_Java_Control_flow/Day_2_Java_Control_flow_Level_1/CalculatorofFactorial_for_loop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CalculatorofFactorial_for_loop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            long factorial = 1;
+
+            for (int i = 1; i <= number; i++) {
+                factorial *= i;
+            }
+
+            System.out.println("The factorial of " + number + " is " + factorial);
+        }
+
+        scanner.close();
+    }
+}
