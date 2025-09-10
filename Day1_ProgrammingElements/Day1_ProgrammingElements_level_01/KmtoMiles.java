57df968ee5e7fc77fa3fe72ae1514c12272df33e
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-10 18:58:23 +0530

    First commit

---
+++ Day1_ProgrammingElements/Day1_ProgrammingElements_level_01/KmtoMiles.java
@@ -0,0 +1,12 @@
+package javaprogrammingElement_level_01;
+
+public class KmtoMiles {
+     public static void main(String[] args) {
+        double Km = 10.8;
+
+        double Miles = Km * 0.62;
+
+        System.out.println("The distance "+ Km +"in miles is "+ Miles);
+    }
+    
+}
