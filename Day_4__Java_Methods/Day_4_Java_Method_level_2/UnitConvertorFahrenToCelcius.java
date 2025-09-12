a1ab963f5ccb2b8d4999b81c240b4e081a2f008e
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-12 22:47:21 +0530

    First commit

---
+++ Day_4__Java_Methods/Day_4_Java_Method_level_2/UnitConvertorFahrenToCelcius.java
@@ -0,0 +1,25 @@
+public class UnitConvertorFahrenToCelcius {
+    public static double convertFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static double convertCelsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double convertPoundsToKilograms(double pounds) {
+        return pounds * 0.453592;
+    }
+
+    public static double convertKilogramsToPounds(double kilograms) {
+        return kilograms * 2.20462;
+    }
+
+    public static double convertGallonsToLiters(double gallons) {
+        return gallons * 3.78541;
+    }
+
+    public static double convertLitersToGallons(double liters) {
+        return liters * 0.264172;
+    }
+}
