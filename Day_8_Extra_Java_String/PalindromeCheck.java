2eef54a8e542cbd2547bda610cb9ded0ea72db53
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-12 23:48:01 +0530

    First commit

---
+++ Day_8_Extra_Java_String/PalindromeCheck.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String r="";
+        for(int i=s.length()-1;i>=0;i--) r+=s.charAt(i);
+        System.out.println(s.equals(r)?"Palindrome":"Not Palindrome");
+    }
+}
