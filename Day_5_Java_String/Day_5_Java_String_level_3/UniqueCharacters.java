929bfa23f6d11a85229b5146eb3f981313a13f08
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-12 23:43:23 +0530

    First commit

---
+++ Day_5_Java_String/Day_5_Java_String_level_3/UniqueCharacters.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class UniqueCharacters {
+    public static int findLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+        }
+        return count;
+    }
+
+    public static char[] uniqueChars(String text) {
+        int n = findLength(text);
+        char[] temp = new char[n];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            char c = text.charAt(i);
+            boolean unique = true;
+            for (int j = 0; j < i; j++) {
+                if (text.charAt(j) == c) {
+                    unique = false;
+                    break;
+                }
+            }
+            if (unique) {
+                temp[index++] = c;
+            }
+        }
+        char[] result = new char[index];
+        for (int i = 0; i < index; i++) result[i] = temp[i];
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        char[] result = uniqueChars(text);
+        for (char c : result) System.out.print(c + " ");
+    }
+}
