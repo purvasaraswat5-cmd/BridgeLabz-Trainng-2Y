1869b18335d8fceabcf270ca5db50a10a389b2fa
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-12 22:53:21 +0530

    First commit

---
+++ Day_4__Java_Methods/Day_4_Java_method_level_3/CollinearCheck.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class CollinearCheck {
+    public static boolean areCollinearSlope(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double slopeAB = (double)(y2 - y1) / (x2 - x1);
+        double slopeBC = (double)(y3 - y2) / (x3 - x2);
+        double slopeAC = (double)(y3 - y1) / (x3 - x1);
+        return slopeAB == slopeBC && slopeBC == slopeAC;
+    }
+
+    public static boolean areCollinearArea(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+        return area == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+        System.out.println("Collinear by Slope: " + areCollinearSlope(x1, y1, x2, y2, x3, y3));
+        System.out.println("Collinear by Area: " + areCollinearArea(x1, y1, x2, y2, x3, y3));
+    }
+}
