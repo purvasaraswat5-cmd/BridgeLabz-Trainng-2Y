00298475e66d296bee166bee54113551a459a8f1
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-12 22:55:41 +0530

    First commit

---
+++ Day_4__Java_Methods/Day_4_Java_method_level_3/FactorChecker.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class FactorChecker {
+    public static int[] findFactors(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) if (n % i == 0) count++;
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= n; i++) if (n % i == 0) factors[index++] = i;
+        return factors;
+    }
+
+    public static int greatestFactor(int[] factors) {
+        int max = factors[0];
+        for (int f : factors) if (f > max) max = f;
+        return max;
+    }
+
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += f;
+        return sum;
+    }
+
+    public static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int f : factors) product *= f;
+        return product;
+    }
+
+    public static long productOfCubes(int[] factors) {
+        long product = 1;
+        for (int f : factors) product *= Math.pow(f, 3);
+        return product;
+    }
+
+    public static void main(String[] args) {
+        int num = 12;
+        int[] factors = findFactors(num);
+        System.out.println("Factors: " + Arrays.toString(factors));
+        System.out.println("Greatest Factor: " + greatestFactor(factors));
+        System.out.println("Sum of Factors: " + sumOfFactors(factors));
+        System.out.println("Product of Factors: " + productOfFactors(factors));
+        System.out.println("Product of Cubes of Factors: " + productOfCubes(factors));
+    }
+}
