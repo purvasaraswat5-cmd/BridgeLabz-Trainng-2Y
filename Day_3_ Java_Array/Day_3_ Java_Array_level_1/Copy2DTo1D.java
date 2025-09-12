74f4bf3bc997ac0104f0037b49af4c8ad781a0b5
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-12 21:53:47 +0530

    First commit

---
+++ Day_3_ Java_Array/Day_3_ Java_Array_level_1/Copy2DTo1D.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Copy2DTo1D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Enter element [" + i + "][" + j + "]: ");
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] flatArray = new int[rows * cols];
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                flatArray[index++] = matrix[i][j];
+            }
+        }
+
+        System.out.print("1D Array: ");
+        for (int i = 0; i < flatArray.length; i++) {
+            System.out.print(flatArray[i] + " ");
+        }
+        System.out.println();
+    }
+}
