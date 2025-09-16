7239dae1596b24ae0cc63eb1d351e5b07fcff4dc
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-16 19:29:30 +0530

    First commit

---
+++ Java_Class_and_Object/Java_Class_and_Object_Level_01/Employee_Details.java
@@ -0,0 +1,24 @@
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Rahul", 101, 45000.50);
+        e1.displayDetails();
+    }
+}
