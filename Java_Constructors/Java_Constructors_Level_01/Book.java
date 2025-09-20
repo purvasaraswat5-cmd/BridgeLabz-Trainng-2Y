bb1466ab423f4428f1e9896aea42567ba986fcf5
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-09-20 10:01:02 +0530

    First commit

---
+++ Java_Constructors/Java_Constructors_Level_01/Book.java
@@ -0,0 +1,28 @@
+class Book {
+    String title;
+    String author;
+    double price;
+
+    Book() {
+        title = "";
+        author = "";
+        price = 0.0;
+    }
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Title: " + title + ", Author: " + author + ", Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book b2 = new Book("Java Programming", "James Gosling", 550.75);
+        b1.display();
+        b2.display();
+    }
+}
