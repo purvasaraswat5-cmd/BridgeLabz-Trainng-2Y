275c03132d728ba2ddd7a79bec71fb08e1b3b2d8
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-10-26 20:25:27 +0530

    First commit

---
+++ Packages_in_java/Package Hierarchy/MainApp.java
@@ -0,0 +1,8 @@
+import com.university.department.cse.Course;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Course course = new Course("Object-Oriented Programming", "CSE101", 4);
+        course.displayCourseDetails();
+    }
+}
