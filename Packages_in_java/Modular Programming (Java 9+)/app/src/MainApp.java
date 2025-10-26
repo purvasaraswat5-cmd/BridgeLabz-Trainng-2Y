9eac085da2ef5b0438a1b5c094121551b46d4c21
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-10-26 20:36:42 +0530

    First commit

---
+++ Packages_in_java/Modular Programming (Java 9+)/app/src/MainApp.java
@@ -0,0 +1,8 @@
+import college.student.Student;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Student student = new Student("Ravi Kumar", 101);
+        student.displayDetails();
+    }
+}
