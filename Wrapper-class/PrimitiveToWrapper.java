f9a9df36a86e32f151bb4fa860e8c78d50658b20
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-11-03 17:50:20 +0530

    First commit

---
+++ Wrapper-class/PrimitiveToWrapper.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class PrimitiveToWrapper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        Integer obj = Integer.valueOf(num);
+        System.out.println("Primitive: " + num);
+        System.out.println("Wrapper: " + obj);
+    }
+}
