c9f0620800acac0b637c2c8da9c6420c13322547
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-11-03 17:56:52 +0530

    First commit

---
+++ Wrapper-class/ScoreBoard.java
@@ -0,0 +1,12 @@
+class ScoreBoard {
+    public static void main(String[] args){
+        Integer[] scores={10,null,20,null,30};
+        int nullCount=0,total=0;
+        for(Integer s:scores){
+            if(s==null)nullCount++;
+            else total+=s;
+        }
+        System.out.println("Players not played: "+nullCount);
+        System.out.println("Total score: "+total);
+    }
+}
