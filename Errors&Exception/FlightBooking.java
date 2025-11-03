3457c805f014c0d2f25e837d83d8fcca3bb9ff42
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-11-03 17:42:53 +0530

    First commit

---
+++ Errors&Exception/FlightBooking.java
@@ -0,0 +1,17 @@
+class SeatUnavailableException extends Exception { SeatUnavailableException(String m){super(m);} }
+class PaymentFailedException2 extends Exception { PaymentFailedException2(String m){super(m);} }
+class FlightBooking {
+    void checkSeatAvailability() throws SeatUnavailableException { throw new SeatUnavailableException("No seats available"); }
+    void processPayment() throws PaymentFailedException2 { throw new PaymentFailedException2("Payment failed"); }
+    void bookTicket() throws Exception {
+        checkSeatAvailability();
+        processPayment();
+    }
+    public static void main(String[] args) {
+        FlightBooking f = new FlightBooking();
+        try { f.bookTicket(); }
+        catch (SeatUnavailableException e) { System.out.println(e.getMessage()); }
+        catch (PaymentFailedException2 e) { System.out.println(e.getMessage()); }
+        finally { System.out.println("Thank you for booking with us!"); }
+    }
+}
