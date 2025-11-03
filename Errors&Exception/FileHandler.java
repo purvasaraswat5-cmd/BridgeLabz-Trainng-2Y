09fc40118381af8480b1096e96704d6598c7ccbf
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-11-03 17:44:44 +0530

    First commit

---
+++ Errors&Exception/FileHandler.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class FileHandler {
+    FileHandler(String file) throws IOException {
+        if (!new File(file).exists()) throw new IOException("File missing");
+    }
+}
+public class TestFile {
+    public static void main(String[] args) {
+        try { new FileHandler("nofile.txt"); }
+        catch (IOException e) { System.out.println(e.getMessage()); }
+    }
+}
