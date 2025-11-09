005f5450c6b814d5a2d7f5ba808147ef8e389022
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-11-09 15:06:24 +0530

    [Purva saraswat /52]add.Create a generic method sumNumbers(List<? extends Number> list) that calculates the sum of all numbers in a list.

---
+++ generics/NumericSum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class NumericSum {
+    public static double sumNumbers(List<? extends Number> list) {
+        double sum = 0;
+        for (Number n : list) sum += n.doubleValue();
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> ints = Arrays.asList(1, 2, 3);
+        List<Double> doubles = Arrays.asList(1.5, 2.5);
+        System.out.println(sumNumbers(ints));
+        System.out.println(sumNumbers(doubles));
+    }
+}
