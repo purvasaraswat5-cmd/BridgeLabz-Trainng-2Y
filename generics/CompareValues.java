bd29db49d303aa2d1f529d945c9843e88fa79c74
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-11-09 15:03:27 +0530

    [Purva saraswat /52]add.Createing a generic method isEqual()  that checks if two values are equal using .equals().

---
+++ generics/CompareValues.java
@@ -0,0 +1,10 @@
+public class CompareValues {
+    public static <T> boolean isEqual(T a, T b) {
+        return a.equals(b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEqual(5, 5));
+        System.out.println(isEqual("Hello", "World"));
+    }
+}
