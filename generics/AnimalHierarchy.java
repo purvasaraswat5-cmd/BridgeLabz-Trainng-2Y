b46e32728d1e19a585fdf9f1e710dc618e0e26f6
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-11-09 15:08:36 +0530

    [Purva saraswat /52]add.Write a method printAnimals(List<? extends Animal> animals) that can print both List<Dog> and List<Cat>.

---
+++ generics/AnimalHierarchy.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Animal {}
+class Dog extends Animal {}
+class Cat extends Animal {}
+
+public class AnimalHierarchy {
+    public static void printAnimals(List<? extends Animal> animals) {
+        for (Animal a : animals) System.out.println(a.getClass().getSimpleName());
+    }
+
+    public static void main(String[] args) {
+        List<Dog> dogs = Arrays.asList(new Dog(), new Dog());
+        List<Cat> cats = Arrays.asList(new Cat());
+        printAnimals(dogs);
+        printAnimals(cats);
+    }
+}
