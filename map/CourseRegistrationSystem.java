557ae4f95d93e4ba8dd9db42273191710b302fab
purvasaraswat5-cmd <purvasaraswat5@gmail.com>
2025-11-17 18:33:38 +0530

    [

---
+++ map/CourseRegistrationSystem.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class CourseRegistrationSystem {
+    public static void main(String[] args) {
+        Map<String,Integer> course = new HashMap<>();
+
+        course.put("CS101",45);
+        course.put("CS102",5);
+        course.put("CS103",52);
+        course.put("CS104",2);
+        course.put("CS105",60);
+
+        course.put("CS102", course.get("CS102") + 3);
+        course.put("CS104", Math.max(0, course.get("CS104") - 1));
+
+        for (String k : course.keySet())
+            if (course.get(k) >= 50) System.out.println(k);
+
+        for (String k : course.keySet())
+            if (course.get(k) < 5) System.out.println(k);
+    }
+}
